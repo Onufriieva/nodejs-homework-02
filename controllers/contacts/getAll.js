3cbc3db7322b816bc174cd26b269fa82981b4c04
Onufriieva <onufrieva.julia@gmail.com>
2022-12-16 19:23:37 +0200

    Зробила пагінацію

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -4,7 +4,9 @@ const getAll = async(req, res, next) => {
 
     try {
         const {_id} = req.user;
-        const contacts = await Contact.find({owner: _id}).populate("owner", "_id email");
+        const { page = 1, limit = 2 } = req.query;
+        const skip = (page - 1) * limit;
+        const contacts = await Contact.find({owner: _id}, "", {skip, limit: Number(limit)}).populate("owner", "_id email");
         res.json({
           status: "success",
           code: 200,
